1803efd18aeaf3b0d6e16c71dddc38aaf0be9092
denisekelsey <dkelsey@bellsouth.net>
2017-10-31 16:02:10 +0000

    Gram detail page

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -16,6 +16,13 @@ class GramsController < ApplicationController
   else
     render :new, status: :unprocessable_entity
   end
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
+  end
 end
 
   private
